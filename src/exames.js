fcbd3c23d6ba6094e357c61775f74c2faa4f12d7
oiuuoiuiououiouiououuoi <wilianpaiva11@gmail.com>
2022-04-09 21:51:02 -0300

    --- Auto-Git Commit ---

--- src/exames.js
+++ src/exames.js
@@ -1,9 +1,15 @@
 import months from './months.js'
 
-let toPrint = months.filter( (months)=> {
+ const days31 = months.filter( (months)=> {
     return months.days === 30
 })
 
-console.log(toPrint)
+console.log(filterMonths)
 
-document.getElementById('main').innerHTML = toPrint.month
+let toPrint = ''
+
+days31.forEach(days => {
+   toPrint += ', ' + days.months
+});
+
+document.getElementById('main').innerHTML = toPrint
