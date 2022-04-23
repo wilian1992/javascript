4db1b21767d08c67f1f8c360f6496a4a4f867911
oiuuoiuiououiouiououuoi <wilianpaiva11@gmail.com>
2022-04-22 22:19:32 -0300

    exercicio ebac

---
+++ src/imobiliaria.js
@@ -0,0 +1,63 @@
+let casas = [
+    {
+
+         id:1,
+         casa: 'casa',
+         Nunquartos:'4',
+         Nunbanheiros:'6',
+         Espaçogaragem:'3',
+         Piscinaquecida:'1',
+         varanda: '1',
+         EspaçoArea: '360m2',
+
+    },
+    {
+
+        id:2,
+        casa: 'sobrado',
+        Nunquartos:'5',
+        Nunbanheiros:'8',
+        Espaçogaragem:'4',
+        Piscinaquecida:'1',
+        varanda: '1',
+        EspaçoArea: '500m2',
+
+   },
+   {
+
+    id:3,
+    casa: 'apartamento',
+    Nunquartos:'2',
+    Nunbanheiros:'2',
+    Espaçogaragem:'1',
+    Piscinaquecida:'1',
+    varanda: '1',
+    EspaçoArea: '60m2',
+
+},
+{
+
+    id:4,
+    casa: 'pequena',
+    Nunquartos:'1',
+    Nunbanheiros:'1',
+    Espaçogaragem:'1',
+    Piscinaquecida:'0',
+    varanda: '1',
+    EspaçoArea: '160m2',
+
+},
+{
+
+    id:5,
+    casa: 'Kitnete',
+    Nunquartos:'1',
+    Nunbanheiros:'1',
+    Espaçogaragem:'0',
+    Piscinaquecida:'0',
+    varanda: '1',
+    EspaçoArea: '40m2',
+
+},
+
+]
